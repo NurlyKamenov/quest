db4880e06c587fb71b85340d3c04291f2d1464ae
NurlyKamenov <nurly1983@gmail.com>
2023-10-02 17:32:37 +0600

    Soft deadline

--- src/test/java/kz/javarush/kamenov/quest/controller/QuestServletTest.java
+++ src/test/java/kz/javarush/kamenov/quest/controller/QuestServletTest.java
@@ -1,12 +1,34 @@
 package kz.javarush.kamenov.quest.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
 
 class QuestServletTest {
+    private final static String questJSP = "WEB-INF/quest.jsp";
 
     @Test
-    void doGet() {
+    void doGet() throws ServletException, IOException {
+        final  QuestServlet servlet= new QuestServlet();
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HttpServletResponse response = mock(HttpServletResponse.class);
+        final HttpSession session = mock(HttpSession.class);
+        final RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+
+        when(request.getSession()).thenReturn(session);
+        when(request.getRequestDispatcher(questJSP)).thenReturn(dispatcher);
+
+        servlet.doGet(request,response);
+
+        verify(request).getRequestDispatcher(questJSP);
+
     }
 }
--- src/test/java/kz/javarush/kamenov/quest/controller/RestartServletTest.java
+++ src/test/java/kz/javarush/kamenov/quest/controller/RestartServletTest.java
@@ -1,12 +1,32 @@
 package kz.javarush.kamenov.quest.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class RestartServletTest {
 
     @Test
-    void doGet() {
+    void doGet() throws ServletException, IOException {
+        final  RestartServlet servlet= new RestartServlet();
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        final HttpServletResponse response = mock(HttpServletResponse.class);
+        final HttpSession session = mock(HttpSession.class);
+
+
+        when(request.getSession()).thenReturn(session);
+
+        servlet.doGet(request,response);
+
+        verify(request).getSession();
+        verify(response).sendRedirect("/");
     }
 }
